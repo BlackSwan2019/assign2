751105367cc9938e6f8fcb9acfc3613dde9c6e33
Ben <liftport@gmail.com>
2018-03-08 10:51:02 -0600

    Final version

--- MilesApp.java
+++ MilesApp.java
@@ -15,8 +15,18 @@ Purpose:      Shows which destinations you can fly to using your air miles; if p
 
 public class MilesApp {
   public static void main(String[] args) {
+    MilesGUI app;   // application window and GUI
+
+    // Make sure a file is supplied at the command line.
+    if (args.length != 1) {
+      System.out.println("Incorrect number of arguments.");
+      System.out.println("Please input only a path to a text file.");
+
+      System.exit(1);
+    }
+
     // Create MilesGUI object.
-    MilesGUI app = new MilesGUI(args[0]);
+    app = new MilesGUI(args[0]);
 
     // Make, populate, and begin app.
     app.go();
